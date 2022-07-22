d3b3dd0ab3373455967aa3ccbb28c09720a06977
Kwnaghcun Lee <kwangchun.lee.7@gmail.com>
2022-07-22 21:06:54 +0900

    start sido moving

--- moving_kosis.R
+++ moving_kosis.R
@@ -64,9 +64,10 @@ pop_2021_sido <- pop_2021_raw %>%
   select(연도, 행정구역명, 인구수 = 합계)
 
 
-# 3. 기초통계 분석 --------------------------------------------------------------
+# 3. 기초통계 분석 -------------------------------------------------------------
+## 3.1. 시도 이사비율 ----------------------------------------------------------
 
-moving_2021_lvl_01 %>% 
+moving_2021_lvl_01_tbl <- moving_2021_lvl_01 %>% 
   left_join(sido_cd, by = c("전입지" = "시도")) %>% 
   left_join(pop_2021_sido, by = c("시도명" = "행정구역명")) %>% 
   select(연도, 전출지, 전입지, 인구수, 전입자수=계) %>% 
@@ -74,7 +75,96 @@ moving_2021_lvl_01 %>%
   arrange(desc(이사비율)) %>%
   distinct()
 
+moving_2021_lvl_01_tbl
 
+## 3.2. 시도 이사비율 시각화 ---------------------------------------------------
+
+moving_2021_lvl_01_tbl_viz <- moving_2021_lvl_01_tbl %>% 
+  select(전입지, 인구수, 전입자수, 이사비율) %>% 
+  pivot_longer(cols = c(인구수, 전입자수)) 
+
+moving_2021_lvl_01_tbl_viz_g <- moving_2021_lvl_01_tbl_viz %>% 
+  ggplot(aes(x = fct_reorder(전입지, value), y = value, fill = name, 
+             width = ifelse(name == "전입자수", 0.3, 0.5))) +
+    geom_col(stat = "identity") +
+    theme_bw(base_family = "NanumGothic") +
+    theme(legend.position = "top",
+          axis.text.y = element_text(size = rel(1.5), colour = "gray35", family = "NanumBarunpen", face="bold"),
+          axis.text.x = element_text(size = rel(1.0), colour = "black", family = "NanumBarunpen", face="bold",
+                                     angle = 15, vjust = 0.5, hjust=0.5),
+          strip.background=element_rect(fill="gray95"),
+          plot.title=element_text(size=18, face="bold", family = "NanumBarunpen"),
+          plot.subtitle=element_text(face="bold", size=13, colour="grey10", family = "NanumBarunpen")) +
+    labs(x        = "",
+         y        = "",
+         title    = "시도별 이동인구수",
+         subtitle = "2021년 인구수/전입자수 [이사비율]",
+         caption  = "자료 출처: 국가통계포털(KOSIS), https://kosis.kr/publication/publicationThema.do",
+         fill     = "") +
+    scale_y_continuous(labels = scales::comma) +
+    coord_flip() +
+    scale_fill_manual(values = c("gray77", "darkblue", "red")) +
+    geom_rect(aes(ymin=14000000, ymax=15500000, xmin=-Inf, xmax=Inf), fill="gray90") +
+    geom_text(data = moving_2021_lvl_01_tbl_viz , 
+              aes(x = 전입지, y = 14750000, 
+                  label=glue::glue("[ {scales::percent(이사비율, accuracy = 0.1)} ]")), 
+              fontface="bold", size=5, family="NanumBarunPen") 
+
+moving_2021_lvl_01_tbl_viz_g
+
+ragg::agg_png(glue::glue("module/images/moving_2021_lvl_01_tbl_viz_g.png"), width = 297, height = 210, units = "mm", res = 600)
+moving_2021_lvl_01_tbl_viz_g
+dev.off()  
+  
+
+
+# 4. 시도 --> 시도  -----------------------------------------------------------
+
+# 4.1. 기술통계 ---------------------------------------------------------------
+
+moving_2021_lvl_02  %>% 
+  arrange(desc(계)) %>% 
+  mutate(비율 = 계/sum(계),
+         누적비율 = cumsum(비율))
+
+# 4.2. 네트워크 데이터 ----------------------------------------------------------------
+library(tidygraph)
+library(ggraph)
+
+## 4.2.1. 네트워크 데이터 정제: 결점 -----
+
+moving_nodes <- pop_2021_sido %>% 
+  left_join(sido_cd, by = c("행정구역명" = "시도명")) %>% 
+  select(id = 시도, 인구수, 구분) %>% 
+  mutate(인구수 = 인구수/10^4)
+
+## 4.2.2. 네트워크 데이터 정제: 연결선 -----
+
+moving_edge <- moving_2021_lvl_02 %>%
+  rename(from = 전출지,
+         to   = 전입지,
+         이사인구수 = 계) %>% 
+  mutate(이사인구수 = 이사인구수/10^4)
+
+## 4.2.3. 네트워크 데이터 변환(tidygraph) -----
+moving_nw <- tbl_graph(
+  nodes = moving_nodes, edges = moving_edge, directed = TRUE
+)
+
+# 4.3. 네트워크 시각화 --------------------------------------------------
+
+moving_sido_nw_g <- ggraph(moving_nw, layout = "linear", circular = TRUE) +
+  # geom_edge_link(aes(width = 이사인구수), alpha = 0.8) +
+  geom_edge_arc(aes(width = 이사인구수), alpha = 0.3) +
+  geom_edge_loop(aes(width = 이사인구수))+ 
+  scale_edge_width(range = c(0.1, 5)) +
+  geom_node_point(aes(size = 인구수, color = 구분)) +
+  geom_label(aes(x = x, y = y,label = id), nudge_y = 0.1) +
+  theme_graph(base_family="NanumGothic") 
+
+ragg::agg_png(glue::glue("module/images/moving_sido_nw_g.png"), width = 297, height = 210, units = "mm", res = 600)
+moving_sido_nw_g
+dev.off()  
 
 
 
